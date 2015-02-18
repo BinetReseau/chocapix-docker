c0ae18a927a1414bed1eef5054b8b01fddce9c12
Nadrieril <nadrieril@gmail.com>
2015-02-18 04:28:40 +0100

    Remove obsolete files

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-docker build -t binetreseau/bars .
-docker run --name="bars-db" -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=django -d mysql
-docker run -p 8000:8000 --name="bars" --link="bars-db:mysqldb" binetreseau/bars
